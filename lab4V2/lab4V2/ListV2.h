e36225637dcc298c38293f907dc658461e3e286b
Will Varner <villiamwarner96@yahoo.com>
2017-10-17 18:55:18 -0400

    Same as the last commit but I added literally two spaces to every line in ListV2. Your'e'e'e'e welcome.

--- lab4V2/lab4V2/ListV2.h
+++ lab4V2/lab4V2/ListV2.h
@@ -3,24 +3,26 @@
 #ifndef LISTV2_H
 #define LISTV2_H
 using namespace std;
- 
-class List{
-   private:
-      typedef struct node{
-          std::string fName, lName;
-          node* next;
-      }* nodePtr;
-      
-      nodePtr head;
-      nodePtr curr;
-      nodePtr temp;
- 
-   public:
-      List();
-      void AddPerson(string addFName, string addLName);
-      void DeletePerson(string delFName, string delLName);
-      void EditPerson(string currFName, string currLName, string editFName, string editLName);
-      void PrintList();
+
+class List
+{
+  private:
+    typedef struct node
+    {
+        std::string fName, lName;
+        node *next;
+    } * nodePtr;
+
+    nodePtr head;
+    nodePtr curr;
+    nodePtr temp;
+
+  public:
+    List();
+    void AddPerson(string addFName, string addLName);
+    void DeletePerson(string delFName, string delLName);
+    void EditPerson(string currFName, string currLName, string editFName, string editLName);
+    void PrintList();
 };
- 
+
 #endif // ListV2_H
