fdd6592acb2a1452e40e27ec5203946568f08408
EchoedEnigma <Malik.z.powell@gmail.com>
2017-11-14 20:28:33 -0500

    Update Electronics.hpp

--- Project 2: Inheritance and Polymorphism/Project 2: Inheritance and Polymorphism/Electronics.hpp
+++ Project 2: Inheritance and Polymorphism/Project 2: Inheritance and Polymorphism/Electronics.hpp
@@ -1,23 +1,62 @@
-//
-//  Electronics.hpp
-//  Project 2: Inheritance and Polymorphism
-//
-//  Created by william wright on 11/2/17.
-//  Copyright © 2017 SilverTap. All rights reserved.
-//
 
 #ifndef Electronics_hpp
 #define Electronics_hpp
 
 #include <stdio.h>
-
-#endif /* Electronics_hpp */
+#include "Item.hpp"
+ /* Electronics_hpp */
+ #endif
 using namespace std;
-class Electronics{
+
+class Electronics : public item{
 private:
+	string brandName;
+	string modelNum;
     string type;
 public:
-    item(string,double);
-    string getname();
-    
+	Electronics();
+	void setType(string t);
+	void setbrandName(string m);
+	void setmodelNum(string b);
+	string gettype();
+	string getmodelNum();
+	string getbrandName();
+	void printItem();
+
+
 };
+Electronics::Electronics()
+{
+	item();
+	type="";
+}
+void Electronics::setType(string t)
+{
+	type = t;
+}
+void Electronics::setmodelNum(string m)
+{
+	modelNum = m;
+}
+void Electronics::setbrandName(string b)
+{
+	brandName = b;
+}
+string Electronics::getbrandName()
+{
+	return brandName;
+}
+string Electronics::getmodelNum()
+{
+	return modelNum;
+}
+string Electronics::gettype()
+{
+	return type;
+}
+void Electronics::printItem()
+{
+	cout <<"Type: "<<type<<endl
+		 <<"Model Number: "<<modelNum<<endl
+		 <<"Brand: "<<brandName<<endl;
+}
